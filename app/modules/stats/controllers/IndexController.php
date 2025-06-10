85bc1df6ac4a24fdd7368b887a6fcfec11af743b
nikfine <nikolay_zinovyev@mail.ru>
2025-06-10 20:08:13 +0300

    cache test

--- app/modules/stats/controllers/IndexController.php
+++ app/modules/stats/controllers/IndexController.php
@@ -13,7 +13,7 @@ class IndexController extends Controller
 {
     public function actionIndex(): array
     {
-        return Yii::$app->cache->getOrSet(serialize($this->request->get()), function () {
+        return Yii::$app->cache->getOrSet(md5(serialize($this->request->get())), function () {
             return new StatsService()->get($this->request->get());
         }, 60) ?? [];
     }
